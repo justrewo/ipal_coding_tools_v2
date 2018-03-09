c32d879a6b845c4aa162e67a94afbce50779110c
justwangjun <forever1984@163.com>
2018-03-09 10:19:58 +0800

    控制真机

--- src/utility/device.utility.ts
+++ src/utility/device.utility.ts
@@ -1,20 +1,53 @@
 import {HTTP} from "@ionic-native/http";
+import {FileTransfer, FileUploadOptions, FileTransferObject} from '@ionic-native/file-transfer';
+import {Platform} from "ionic-angular";
+import {File} from "@ionic-native/file";
 
 export class DeviceUtility {
     private uuid:string = null;
     private url:string;
     private content:string;
     private http:HTTP = new HTTP();
+    private transfer:FileTransfer = new FileTransfer();
+    private platform:Platform = new Platform();
+    private file:File = new File();
 
     constructor(url:string, content:string) {
         this.url = url;
         this.content = content;
     }
 
+    private getFileDirectory() {
+		if (this.platform.is("ios")) {
+			return this.file.documentsDirectory;
+		} else {
+			return this.file.externalDataDirectory;
+		}
+    }
+
+    private getFileName() {
+		return 'runcode.rs';
+    }
+    
+	private getFilePath() {
+		if (this.platform.is("ios")) {
+			let tmp_path = this.getFileDirectory() + this.getFileName();
+			return tmp_path.replace(/^file:\/\//,"");
+		} else {
+			return this.getFileDirectory() + this.getFileName();
+		}
+	}
+
+	private getFilePathRaw() {
+		let tmp_path = this.getFileDirectory() + this.getFileName();
+		return tmp_path.replace(/^file:\/\//,"");
+	}
+    
     public uploadDataToServer(filename, type, callback, userdata) {
         let that = this;
+        
         var xhr = new XMLHttpRequest();
-        xhr.open(/* method */ "POST", this.url + "upload", true, "admin", "admin");
+        xhr.open("POST", this.url + "upload", true);
         xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=---------------------------5081957846815694751082490309");
     
         var body = "-----------------------------5081957846815694751082490309\r\nContent-Disposition: form-data; name=\"fileUploaded\"; filename=\"" + filename + "\"\r\nContent-Type: " + type + "\r\n\r\n" + this.content + "\r\n\r\n-----------------------------5081957846815694751082490309--\r\n";
@@ -34,8 +67,29 @@ export class DeviceUtility {
                 }
             }
         }
+        
 
-        //var options = new FileUploadOptions();
+        /*
+        this.file.createFile(this.getFileDirectory(), this.getFileName(), true);
+        this.file.writeExistingFile(this.getFileDirectory(), this.getFileName(), this.content);
+
+        let options: FileUploadOptions = {
+            fileKey: 'file',
+            fileName: filename,
+            mimeType: "multipart/form-data",
+            params: {
+                fileUploaded:this.content
+            },
+            headers: {}
+         }
+
+         let filetransfer = this.transfer.create();
+         filetransfer.upload(this.getFileDirectory(), this.url + "upload", options, true).then((data)=>{
+            console.log("wangjun" + "aaaaaa");
+         }, (err)=>{
+            console.log("wangjunkkk" + err);
+         });
+         */
     }
 
     private startRunScript(hash) {
